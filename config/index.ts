f8d8d3c8e7ec5d535c46ff6cff0885a9c7c9923b
SnowyField1906 <aswdqe1x@gmail.com>
2024-07-19 19:07:17 +0700

    includes lock file for deployment

--- config/index.ts
+++ config/index.ts
@@ -6,31 +6,10 @@ dotenv.config({ path: isProduction ? '.env' : '.env.local' })
 const nodes = [
     {
         id: 1,
-        url: process.env.NODE1_URL,
+        url: 'https://average-leonora-snowyfield1906-afbf7760.koyeb.app/',
     },
-    {
-        id: 2,
-        url: process.env.NODE2_URL,
-    },
-    {
-        id: 3,
-        url: process.env.NODE3_URL,
-    },
-]
-
-const nodePrivateKeys = [
-    {
-        id: 1,
-        privateKey: process.env.NODE1_PRIVATE_KEY,
-    },
-    {
-        id: 2,
-        privateKey: process.env.NODE2_PRIVATE_KEY,
-    },
-    {
-        id: 3,
-        privateKey: process.env.NODE3_PRIVATE_KEY,
-    },
+    { id: 2, url: '' },
+    { id: 3, url: '' },
 ]
 
 const productionConfig = {
@@ -49,9 +28,7 @@ const localConfig = {
     database: {
         mongoUri: `mongodb+srv://kiet1618:12052002@kltn.mbww3bu.mongodb.net/node${process.env.NODE_ID}?retryWrites=true&w=majority`,
     },
-    privateKey: nodePrivateKeys.find(
-        (node) => node.id === Number(process.env.NODE_ID)
-    ).privateKey,
+    privateKey: process.env[`NODE${process.env.NODE_ID}_PRIVATE_KEY`],
     nodes: nodes,
 }
 
